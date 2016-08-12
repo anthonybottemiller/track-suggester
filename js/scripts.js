1a1e999925f1e25ee9993e8a694754da44036fbf
Anthony Bottemiller <anthonybottemiller@outlook.com>
2016-08-12 11:17:18 -0700

    adds statements to initalize variable for all questions to user input values

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
-  
-})
+//Initialize scoring variables
+  var cSharpScore = 0;
+  var phpScore = 0;
+  var rubyScore = 0;
+  var javaScore = 0;
+  //When form is submitted score survey
+  $("button#submit").click(function(){
+    var answer1 = $("input:radio[name=answer1]:checked").val();
+    console.log(answer1)
+    var answer2 = $("input:radio[name=answer2]:checked").val();
+    console.log(answer2)
+    var answer3 = $("input:radio[name=answer3]:checked").val();
+    console.log(answer3)
+    var answer4 = $("input:radio[name=answer4]:checked").val();
+    console.log(answer4)
+    var answer5 = $("input:radio[name=answer5]:checked").val();
+    console.log(answer5)
+
+  });
+});
